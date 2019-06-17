ce4a3ad01411a54fe71c7086aec161cd5a464691
mbstephenson10 <49455242+mbstephenson10@users.noreply.github.com>
2019-06-17 16:03:33 -0400

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -48,7 +48,7 @@ namespace GradeBook.UserInterfaces
                 gradeBook = new RankedGradeBook(name);
             else
             {
-                Console.WriteLine(" is not a supported type of gradebook, please try again.");
+                Console.WriteLine($"{type} is not a supported type of gradebook, please try again.");
                 return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
